9d08569261a603b00e045a4ea44d29c7eeb0cf4a
Gonzalo Chumillas <gonzalo.chumillas@gmail.com>
2025-08-22 23:06:23 +0100

    Refactor ParseStatement function to improve readability by removing unnecessary line breaks

--- parser.go
+++ parser.go
@@ -34,33 +34,25 @@ func ParseStatement(p *parser.Parser, next func(p *parser.Parser) ast.Statement)
 	if p.CurrentToken.Type != token.IDENT || p.CurrentToken.Literal != "try" {
 		return next(p)
 	}
-
 	if !p.ExpectToken(token.LBRACE) {
 		return nil
 	}
-
 	stmt := &TryStatement{}
 	stmt.TryBlock = p.ParseBlockStatement()
-
 	if p.PeekToken.Type == token.IDENT && p.PeekToken.Literal == "catch" {
 		p.NextToken() // consumes catch
-
 		if p.ExpectToken(token.LPAREN) {
 			p.NextToken() // consumes (
 			stmt.CatchParameter = &ast.Identifier{Token: p.CurrentToken, Value: p.CurrentToken.Literal}
 		}
-
 		if !p.ExpectToken(token.RPAREN) {
 			return nil
 		}
-
 		if !p.ExpectToken(token.LBRACE) {
 			return nil
 		}
-
 		stmt.CatchBlock = p.ParseBlockStatement()
 	}
-
 	if p.PeekToken.Type == token.IDENT && p.PeekToken.Literal == "finally" {
 		p.NextToken() // consumes finally
 		if !p.ExpectToken(token.LBRACE) {
@@ -68,6 +60,5 @@ func ParseStatement(p *parser.Parser, next func(p *parser.Parser) ast.Statement)
 		}
 		stmt.FinallyBlock = p.ParseBlockStatement()
 	}
-
 	return stmt
 }
